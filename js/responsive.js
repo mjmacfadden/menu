6b69fe69c1cff6ea3b797f0d45de503f53b451d6
Kevin Glowacz <kevin@glowacz.info>
2020-02-14 09:52:16 -0600

    Alternate implementation with less js and more css

--- js/responsive.js
+++ js/responsive.js
@@ -1,17 +1,10 @@
 $( document ).ready(function() {
-    var menu_button = false;
-    $( "#column-one" ).prepend( "<img src='images/menu.png' id='menu' onclick='' />" );     //This adds the menu button
-    $('#menu').click(function(){                                                            //This opens and closes the menu
-        if (menu_button == false){
-            $('#page-navigation').animate({'max-height':'500px'}, 100);
-            menu_button = true;
-        }else{
-            $('#page-navigation').animate({'max-height':'0px'}, 100);
-            menu_button = false;
-        }
+    $( "#column-one" ).prepend( "<img src='images/menu.png' id='menu'/>" );     //This adds the menu button, we can add this as hidden element to all sites, or just responsive templates
+    $('#menu').click(function(){
+        $('#page-navigation').toggleClass("open")
     });
     $("#header-name").click(function(event){                                                //This turns the site name into a clickable link.
-        window.location = "/menu/";                                                         //This specific implementation would not work on a celect.org website                                                      
+        window.location = "/menu/";                                                         //This specific implementation would not work on a celect.org website
     });
 
 });
